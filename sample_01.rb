b7f50526c1cc4ce877a51cef2693d21d3327a55d
y_tsubasa <tbs_chako@yahoo.co.jp>
2020-01-07 23:23:34 +0900

    式の返り値とメソッドの返り値について学習する

---
+++ sample_01.rb
@@ -0,0 +1,25 @@
+#####
+# 最後に評価された式が、そのメソッドの返り値となる。
+#####
+
+# while文は値を返さない式である。breakなどで意図的に値を返さないなら、必ずnilを返す。
+def while_only
+  num = 0
+  while num < 2 do
+    num += 1
+  end
+end
+
+# if文は値を返す式である。if文の中に入らない場合はnilを返す。
+def if_only
+  name = ""
+  if true
+    name = "Tsubasa"
+  end
+  if false
+    name = "Tsubasa"
+  end
+end
+
+puts while_only.nil? # true
+puts if_only.nil? # true
