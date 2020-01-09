ac2c10d4391d3206b1177eb5f844af3742473715
y_tsubasa <tbs_chako@yahoo.co.jp>
2020-01-09 23:39:52 +0900

    ％記法について学習する

---
+++ sample_02.rb
@@ -0,0 +1,11 @@
+array = %w(one two three four)
+p array
+# => ["one", "two", "three", "four"]
+
+array = %w(1 2 3 4)
+p array
+# => ["1", "2", "3", "4"]
+
+array = %i(Tsubasa Yoshida Ruby)
+p array
+# => [:Tsubasa, :Yoshida, :Ruby]
